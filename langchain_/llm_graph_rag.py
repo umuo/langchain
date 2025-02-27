3929b6ab1e8650c845e0862fa67ec03acf595c81
nbh <gitsilence@gmail.com>
2025-02-27 17:28:29 +0800

    update

--- langchain_/llm_graph_rag.py
+++ langchain_/llm_graph_rag.py
@@ -3,15 +3,20 @@ from langchain_community.graphs.nebula_graph import NebulaGraph
 from langchain.chains import NebulaGraphQAChain
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+import os
+
 # 按优先级依次加载配置文件
 load_dotenv('.env', override=True)            # 最低优先级
 load_dotenv('.env.local', override=True)      # 最高优先级
 
+API_KEY = os.getenv("OPENAI_API_KEY")
+BASE_URL = os.getenv("OPENAI_BASE_URL")
+CHAT_MODEL = os.getenv("CHAT_MODEL_NAME")
 
 llm = ChatOpenAI(
-    model="moonshot-v1-8k",
-    api_key="sk-clHCmfujSb1xvjOfQpB7Ojp7VtWfZVHmYjaGp8o0xdFfvz3Y",
-    base_url="http://10.118.21.135:3000/v1"
+    model=CHAT_MODEL,
+    api_key=API_KEY,
+    base_url=BASE_URL
 )
 graph_db = NebulaGraph(
     space="nezha2",
