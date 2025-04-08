2bb3f3c102f1a622227ad513a73adad4a4b8f8f2
gitsilence <gitsilence@lacknb.cn>
2025-04-09 00:14:54 +0800

    update

--- langgraph_/chatbot_stu_01.py
+++ langgraph_/chatbot_stu_01.py
@@ -4,14 +4,19 @@ from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from base.base_llm import llm
 import asyncio
+from dotenv import load_dotenv
+load_dotenv("../.env.local")
+
 # from IPython.display import Image, display
 
 class State(TypedDict):
     # message的类型是list，通过add_messages函数将消息添加到messages
     messages: Annotated[list, add_messages]
 
+
 def chatbot(state: State) -> dict:
     return {"messages": llm.invoke(state["messages"])}
+
 
 # 将State作为输入，返回一个包含messages键 messages下更新列表的字典，这是langgraph节点函数的基本模式
 graph_builder = StateGraph(State)
@@ -27,14 +32,23 @@ graph = graph_builder.compile()
 
 async def stream_graph_updates(user_input: str):
     import json
-    events = [event async for event in graph.astream_events({"messages": [("user", user_input)]}, version="v2")] 
+    events = [event async for event in graph.astream_events({"messages": [("user", user_input)]}, version="v2")]
     for event in events:
         # print(event)
         if event["event"] == "on_chain_stream" and event["name"] == "chatbot":
             print("AI:", event["data"]["chunk"]['messages'])
     # for event in graph.stream({"messages": [("user", user_input)]}):
-        # for value in event.values():
-            # print("AI:", value["messages"])
+    # for value in event.values():
+    # print("AI:", value["messages"])
+
+
+async def graph_updates(user_input: str):
+    response = graph.invoke({"messages": [("user", user_input)]}, debug=True)
+    print(response["messages"])
+    for message in response["messages"]:
+        print(message.content)
+
+
 async def main():
     while True:
         try:
@@ -42,11 +56,15 @@ async def main():
             if user_input in ("exit", "quit", "q"):
                 print("Goodbye!")
                 break
-            await stream_graph_updates(user_input)
-        except:
+            # await stream_graph_updates(user_input)
+            await graph_updates(user_input)
+        except Exception as e:
+            print(f"Error: {e}")
             user_input = "你想知道关于langgraph的什么？"
             await stream_graph_updates(user_input)
             break
+
+
 asyncio.run(main())
 # try:
 #     # get_graph您可以使用方法和其中一种“绘制”方法（例如draw_ascii或 ）来可视化图形draw_png 
